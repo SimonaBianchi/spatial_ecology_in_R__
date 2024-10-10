e3ebe90b0a768f1e420d0b32861bc81aef40923c
SimonaBianchi <simona.bianchi2199@gmail.com>
2024-10-10 15:56:42 +0200

    Create Population_Density.R

---
+++ Code/Population_Density.R
@@ -0,0 +1,41 @@
+# How to calculate the density of individuals in population
+
+# Installing the spatstat package
+install.packages ("spatstat")
+
+#Recalling the package
+library (spatstat)
+
+#dataset
+bei
+
+plot (bei)
+
+# Changing the argument
+plot(bei, pch=19)
+
+# cahnging the shape and size
+plot(bei, pch=19, cex=.5)
+
+# covariate images 
+bei.extra
+
+plot(bei.extra)
+
+#extracting datat
+elevation <- bei.extra$elev
+plot(elevation)
+
+elevation <- bei.extra[[1]]
+
+# Density map starting from points
+densitymap <- density(bei)
+
+plot(densitymap)
+plot(bei)
+
+points(bei)
+points(bei, col="green")
+
+
+
