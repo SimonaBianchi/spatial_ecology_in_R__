0e6465266c68cef7a5212767332f5f47101a0893
SimonaBianchi <simona.bianchi2199@gmail.com>
2024-11-13 15:11:06 +0100

    Create 06_remotesensingdatavisualization.R

---
+++ Code/06_remotesensingdatavisualization.R
@@ -0,0 +1,6 @@
+# code for managing and analyzing remote sensing data
+# imageRy --> function to intall from github: intall_github(). this function belong to the package devtools (package for developing packages)
+
+install.packages("devtools")
+library(devtools)
+install_github("ducciorocchini/imageRy")
